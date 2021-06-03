fd9c3af2e0a11fcf0504ee81e1e2b2124a12aa74
farhan0651 <fk912254@gmail.com>
2021-06-04 03:08:37 +0530

    scaffolded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss'
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
